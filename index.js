64976c68086d8e5f3049a6b5bb6e8c19e951d156
unknown <anubhavdixit268@gmail.com>
2023-06-07 22:52:14 +0530

    trainapp

--- index.js
+++ index.js
@@ -1,24 +1,40 @@
 const express = require("express");
 const connection = require("./config/db");
 const cors = require("cors");
-const userRouter = require("./routes/user.routes");
-const ringRouter = require("./routes/ring.routes");
-const earringRouter = require("./routes/earring.routes");
-const cartRouter = require("./routes/cart.routes");
-const {userAuth} = require("./middlewares/authorization");
-const searchRouter = require("./routes/search.routes");
+const seatsRouter = require("./routes/seats.routes");
 require("dotenv").config();
 
 const app = express();
+
 app.use(cors());
+
 app.use(express.json());
 
-app.use("/users", userRouter);
-app.use("/rings", ringRouter);
-app.use("/earrings", earringRouter);
-app.use("/search",searchRouter)
-app.use(userAuth);
-app.use("/cart", cartRouter);
+app.use("/seats", seatsRouter);
+
+app.get("/", (req, res) => {
+  res.send(
+    `<u>
+      <h1 style="text-align:center;margin:50px;">Train Seats Reservation API</h1>
+    </u>
+    <h2 style="text-align:center;margin:50px;">
+      GET : <a href="http://localhost:8080/seats"> /seats</a>
+    </h2>
+    <h2 style="text-align:center;margin:50px;">
+      PATCH : <a href="http://localhost:8080/seats/reserve"> /seats/reserve</a>
+    </h2>
+    <h2 style="text-align:center;margin-left:-500px;" >Instructions :- </h2>
+    <h3 style="width:fit-content;margin:auto">
+      <ol >
+        <li>User can book the seats at  [ /seats/reserve ].</li>
+        <li>You have to provide the number of required seats in body of the request.</li>
+        <li>An user can book upto 7 seats at a time. </li>
+        <li>In the backend priority will be to book seats in single row.</li>
+        <li>If required seats are not available i single row, seats will be booked in nearby rows.</li>
+      </ol>
+    </h3>`
+  );
+});
 
 app.listen(process.env.port, async () => {
   try {
